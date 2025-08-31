409e3701e9f0dbfffdc0d2850f65f14ca58f032d
kavankfc <77088561+kavankfc@users.noreply.github.com>
2025-08-30 22:46:33 -0700

    updated dependencies (#3)
    * updated dependencies
    * update github action yaml

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,3 @@
 poetry run autoflake hk1980 --remove-all-unused-imports --ignore-init-module-imports --remove-duplicate-keys --remove-unused-variables -i -r
-poetry run black hk1980 -t py39
-poetry run isort hk1980 --profile=black
+poetry run ruff check --select I --fix .
+poetry run ruff format .
--- script/performance.py
+++ script/performance.py
@@ -5,7 +5,7 @@ from hk1980 import LatLon
 
 stmt = "LatLon(22.284034, 114.137814).to_hk80()"
 
-time = timeit.timeit(stmt=stmt, globals=globals(), number=100)*1000
+time = timeit.timeit(stmt=stmt, globals=globals(), number=100) * 1000
 print(f"{time=}  ms")
 
 cProfile.run(stmt)
